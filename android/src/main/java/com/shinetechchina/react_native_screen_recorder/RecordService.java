72dbb1e70b46f078a120fdd4d6676bebb5fcdf99
Asleepace <colin_teahan@yahoo.com>
2018-09-21 15:01:00 -0700

    Update RecordService.java

--- android/src/main/java/com/shinetechchina/react_native_screen_recorder/RecordService.java
+++ android/src/main/java/com/shinetechchina/react_native_screen_recorder/RecordService.java
@@ -10,7 +10,9 @@ import android.media.projection.MediaProjection;
 import android.os.Binder;
 import android.os.Environment;
 import android.os.HandlerThread;
+import android.app.Activity;
 import android.os.IBinder;
+import android.view.Surface;
 import android.widget.Toast;
 
 import java.io.File;
@@ -18,13 +20,16 @@ import java.io.IOException;
 
 
 public class RecordService extends Service {
+
     private MediaProjection mediaProjection;
-    private MediaRecorder mediaRecorder;
+    //private MediaRecorder mediaRecorder;
+    private VideoCapture mediaRecorder;
     private VirtualDisplay virtualDisplay;
 
     private String filePath;
 
     private boolean running;
+    private int maxDuration = 10000;
     private int width = 720;
     private int height = 1080;
     private int dpi;
@@ -47,7 +52,8 @@ public class RecordService extends Service {
                 android.os.Process.THREAD_PRIORITY_BACKGROUND);
         serviceThread.start();
         running = false;
-        mediaRecorder = new MediaRecorder();
+        // mediaRecorder = new MediaRecorder();
+        mediaRecorder = new VideoCapture();
     }
 
     @Override
@@ -74,7 +80,7 @@ public class RecordService extends Service {
             return false;
         }
 
-        initRecorder();
+        mediaRecorder.initRecorder();
         createVirtualDisplay();
         mediaRecorder.start();
         running = true;
@@ -86,6 +92,7 @@ public class RecordService extends Service {
         {
             return filePath;
         }
+
         running = false;
         mediaRecorder.stop();
         mediaRecorder.reset();
@@ -98,24 +105,6 @@ public class RecordService extends Service {
     private void createVirtualDisplay() {
         virtualDisplay = mediaProjection.createVirtualDisplay("MainScreen", width, height, dpi,
                 DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, mediaRecorder.getSurface(), null, null);
-    }
-
-    private void initRecorder() {
-        filePath = getsaveDirectory() + System.currentTimeMillis() + ".mp4";
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        mediaRecorder.setOutputFile(filePath);
-        mediaRecorder.setVideoSize(width, height);
-        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mediaRecorder.setVideoEncodingBitRate(5 * 1024 * 1024);
-        mediaRecorder.setVideoFrameRate(30);
-        try {
-            mediaRecorder.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public String getsaveDirectory() {
@@ -141,4 +130,54 @@ public class RecordService extends Service {
         public RecordService getRecordService() {
             return RecordService.this;
         }
-    }}
+    }
+
+    // Subclass of the MediaRecorder which implements the max duration of what will happen
+    // when the timout is reached. This will stop and save the video.
+    public class VideoCapture extends Activity implements MediaRecorder.OnInfoListener {
+
+        private MediaRecorder recorder = new MediaRecorder();
+
+        public Surface getSurface() {
+            return recorder.getSurface();
+        }
+
+        public void start() {
+            recorder.start();
+        }
+
+        public void stop() {
+            recorder.stop();
+        }
+
+        public void reset() {
+            recorder.reset();
+        }
+
+        public void initRecorder() {
+            filePath = getsaveDirectory() + System.currentTimeMillis() + ".mp4";
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setOutputFile(filePath);
+            recorder.setVideoSize(width, height);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setVideoEncodingBitRate(5 * 1024 * 1024);
+            recorder.setVideoFrameRate(30);
+            recorder.setMaxDuration(maxDuration); // 10 seconds
+            recorder.setOnInfoListener(this);
+            try {
+                recorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+            if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
+                stopRecord();
+            }
+        }
+    }
+  }
